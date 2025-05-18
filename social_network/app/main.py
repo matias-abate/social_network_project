526f3b3667bf2f6d1a538c8c1df21b57d8e47520
lucasRubiniF1 <lrubini@uade.edu.ar>
2025-05-17 21:46:15 -0300

    Primer commit - Clase 9: Stack

--- social_network/app/main.py
+++ social_network/app/main.py
@@ -1,13 +1,26 @@
 # app/main.py
-from fastapi import FastAPI
-from app.routes import user, post, relationship
 
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+neo4j_uri = os.getenv("NEO4J_URI")
+neo4j_user = os.getenv("NEO4J_USER")
+neo4j_pass = os.getenv("NEO4J_PASSWORD")
+redis_host = os.getenv("REDIS_HOST")
+redis_port = int(os.getenv("REDIS_PORT"))
+
+# Inicializar la app de FastAPI
 app = FastAPI()
 
 # Importar las rutas
-app.include_router(user.router, prefix="/users", tags=["Users"])
-app.include_router(post.router, prefix="/posts", tags=["Posts"])
-app.include_router(relationship.router, prefix="/relationships", tags=["Relationships"])
+from app.routes import user, post, relationship, cache
+
+# Registrar rutas
 app.include_router(user.router, prefix="/users", tags=["Users"])
 app.include_router(post.router, prefix="/posts", tags=["Posts"])
 app.include_router(relationship.router, prefix="/relationships", tags=["Relationships"])
